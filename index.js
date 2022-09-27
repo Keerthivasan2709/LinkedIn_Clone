90d80c97832e851e159b446ff48f39a78bd84acd
Keerthivasan2709 <keerthivasan.b@codingmart.com>
2022-09-27 11:11:41 +0530

    profile

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-$("#Me").click(() => {
-    console.log($("#Me"))
-    a = $("#Me")[0].getBoundingClientRect();
-    console.log(a);
-    $('#aboutMeSection').toggle()
-    $('#aboutMeSection')[0].style.left = `${a.x}px`
+$(document).ready(() => {
+    $("#Me").click(() => {
+        console.log($("#Me"))
+        a = $("#Me")[0].getBoundingClientRect();
+        console.log(a);
+        $('#aboutMeSection').toggle()
+        $('#aboutMeSection')[0].style.left = `${a.x}px`
+    });
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 360) {
+            $(".ads-container").addClass('fixed')
+        }
+        else if (window.pageYOffset < 360) {
+            $(".ads-container").removeClass('fixed')
+        }
+    })
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 455) {
+            $("#recent").addClass('recent-fixed')
+        }
+        else if (window.pageYOffset < 455) {
+            $("#recent").removeClass('recent-fixed')
+        }
+
+    })
+    let width=$(".feature").outerWidth();
+    console.log(width)
+    $(".btn-right-arrow").click(() => {
+        $(".features")[0].scrollBy(width*1, 0)
+    })
+
+    $(".btn-left-arrow").click(() => {
+        $(".features")[0].scrollBy(-width*1, 0)
+    })
 });
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 360) {
-        $(".ads-container").addClass('fixed')
-    }
-    else if (window.pageYOffset < 360) {
-        $(".ads-container").removeClass('fixed')
-    }
-})
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 455) {
-        $("#recent").addClass('recent-fixed')
-    }
-    else if (window.pageYOffset < 455) {
-        $("#recent").removeClass('recent-fixed')
-    }
-
-})
-
-let scroll=window.innerHeight<768?300:600;
-
-
-$(".btn-right-arrow").click(() => {
-    $(".features")[0].scrollBy(scroll, 0)
-})
-
-$(".btn-left-arrow").click(() => {
-    $(".features")[0].scrollBy(-scroll, 0)
-})
